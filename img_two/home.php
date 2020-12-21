19926c927f5b7443469cc7e2898b29ac6f8ea3bc
Arif Hasan <69918947+arif-hasan43@users.noreply.github.com>
2020-12-22 01:51:36 +0600

    Create home.php

---
+++ img_two/home.php
@@ -0,0 +1,48 @@
+<?php  
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'testing');
+ 
+ $connect = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($connect === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+ echo "Connected successfully";
+ ?>  
+ 
+ <!DOCTYPE html>  
+ <html>  
+      <head>  
+           <title>Webslesson Tutorial | Insert and Display Images From Mysql Database in PHP</title>  
+           <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
+           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
+           <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
+      </head>  
+      <body>  
+           <br /><br />  
+           <div class="container" style="width:1000px;">  
+                <h3 align="center">Insert and Display Images From Mysql Database in PHP</h3>  
+				<br /><br /> 
+                <?php  
+                $query = "SELECT * FROM img_two ORDER BY id DESC";  
+                $result = mysqli_query($connect, $query);  
+                while($row = mysqli_fetch_array($result))  
+                {  
+                     echo '  
+                          <tr>  
+						<br>
+                               <td> 
+								<br>							   
+                                    <img src="data:image/jpeg;base64,'.base64_encode($row['name'] ).'" height="500" width="1000" class="img-thumnail" />  
+                               </td>  
+                          </tr>  
+                     ';  
+                }  
+                ?>  
+                
+           </div>  
+      </body>  
+ </html>  
